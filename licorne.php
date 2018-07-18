7173ed548ed0946b1a55b115b4f97ea77ca051c2
Hautecoeur <hautecoeurludovic@gmail.com>
2018-07-18 16:12:31 +0200

    review the code ternaire and beautiful

--- licorne.php
+++ licorne.php
@@ -14,13 +14,18 @@
 
     if (isset($_GET['race'])) {
         $race = $_GET['race'];
-       
-        echo($humain = ($race == 'humain') ? '<img src="https://bestof.one/ressources/images/vignette/corps-humain.svg">' : false);
-        echo($chat = ($race == 'chat') ? '<img src="https://i.ytimg.com/vi/py9Xt3wVAu0/maxresdefault.jpg">' : false);
-        echo($licorne = ($race == 'licorne') ? '<img src="https://cms-assets.tutsplus.com/uploads/users/108/posts/27336/image/how-to-draw-unicorn-4-4.png">' : false); 
+
+        // echo($humain = ($race == 'humain') ? '<img src="https://bestof.one/ressources/images/vignette/corps-humain.svg">' : false);
+        // echo($chat = ($race == 'chat') ? '<img src="https://i.ytimg.com/vi/py9Xt3wVAu0/maxresdefault.jpg">' : false);
+        // echo($licorne = ($race == 'licorne') ? '<img src="https://cms-assets.tutsplus.com/uploads/users/108/posts/27336/image/how-to-draw-unicorn-4-4.png">' : false); 
+
+        $humain = '<img src="https://bestof.one/ressources/images/vignette/corps-humain.svg">';
+        $chat = '<img src="https://i.ytimg.com/vi/py9Xt3wVAu0/maxresdefault.jpg">';
+        $licorne = '<img src="https://cms-assets.tutsplus.com/uploads/users/108/posts/27336/image/how-to-draw-unicorn-4-4.png">';
+
+        $reponse = ($race == 'humain') ? $humain  : $reponse =($race == 'chat') ? $chat : $licorne  ;
+        echo($reponse);
 
     }
-
-
 
 ?>
